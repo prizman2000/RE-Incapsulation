7ca46ac734bd722cc026b9acfe0171f740f15217
Slava Boss <prizman2000@mail.ru>
2020-05-26 23:46:57 +1000

    added abstraction

--- main.cpp
+++ main.cpp
@@ -2,66 +2,68 @@
 
 using namespace std;
 
-class Library{
+class Library {
 private:
     string book_name;
     string book_autor;
-    unsigned int shits_value;
+
 protected:
     unsigned int value_of_security;
 public:
+    unsigned int shits_value = 50;
     string adress;  //Отсутствие инкапсуляции
 
-    Library(string adress){
+    Library(string adress) {
         this->adress = adress;
     }
 
-    string getName(){   //Геттер
+    string getName() {   //Геттер
         return book_name;
     }
 
-    void setName(string book_name){  //Сеттер
+    void setName(string book_name) {  //Сеттер
         this->book_name = book_name;
     }
 
-    virtual void setShits_value(unsigned int value){    //Позднее связывание
+    virtual void setShits_value(unsigned int value) {    //Позднее связывание
         this->shits_value = value;
-        cout<<shits_value;
+        cout << shits_value;
     }
 
-    void sayShits_value(){
-        cout<<"Количиство листиков равно: ";
-        setShits_value(50);
 
+    void say_Vezhlivo() {    //Расширение поведения
+        cout << "Можно мне пожалуйста книжечку неизвестного мне автора, которая называется " << getName();
     }
 
-    void say_Vezhlivo(){    //Расширение поведения
-        cout<<"Можно мне пожалуйста книжечку неизвестного мне автора, которая называется "<<getName();
+    virtual void talk() {
+        cout << "...";
     }
 
-    virtual void talk(){
-        cout<<"...";
-    }
+
 
 };
 
-class Librarian : public Library{   //Инкапсуляция при помощи абстракций
+class Librarian : public Library {   //Инкапсуляция при помощи абстракций
 private:
     string name;
     unsigned int age;
     unsigned int experience;
 public:
-    Librarian (string adress, string name, unsigned int age, unsigned int experience) : Library(adress){
+    Librarian(string adress, string name, unsigned int age, unsigned int experience) : Library(adress) {
         this->name = name;
         this->age = age;
         this->experience = experience;
     }
 
     void talk() override {  //Замена поведения
-        cout<<"Hello";
+        cout << "Hello";
     }
-
 };
+
+void abstraction(Library* lib){
+    lib->talk();
+}
+
 
 int main() {
 
@@ -77,6 +79,11 @@ int main() {
     vasya.say_Vezhlivo();
     vasya.getName();
     vasya.adress = "Pereezhau...";
+
+    cout << endl << endl;
+    abstraction(&lib);
+    cout<<endl;
+    abstraction(&vasya);
 
     return 0;
 }
